7857701e89abf865df218c0124e6ad3f3a38bc17
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-01-03 18:59:18 +0000

    Set metavar to "N" for the flags that expect an int

--- flags.py
+++ flags.py
@@ -9,48 +9,48 @@ def parse_arguments(raw_args=None):
     parser.add_argument('--lik', default='TunePrLikelihood', help='Likelihood function')
     parser.add_argument('--lr', default=0.1, type=float, help='Learning rate')
     parser.add_argument('--metrics', default='binary_accuracy', help='List of metrics to log')
-    parser.add_argument('--loo_steps', default=0, type=int,
+    parser.add_argument('--loo_steps', default=0, type=int, metavar='N',
                         help='Number of steps for optimizing LOO loss; 0 disables')
-    parser.add_argument('--nelbo_steps', default=0, type=int,
+    parser.add_argument('--nelbo_steps', default=0, type=int, metavar='N',
                         help='Number of steps for optimizing NELBO loss; 0 means same as loo_steps')
-    parser.add_argument('--num_all', default=200, type=int,
+    parser.add_argument('--num_all', default=200, type=int, metavar='N',
                         help='Suggested total number of examples (datasets don\'t have to use it)')
-    parser.add_argument('--num_train', default=50, type=int,
+    parser.add_argument('--num_train', default=50, type=int, metavar='N',
                         help='Suggested number of train examples (datasets don\'t have to use it)')
-    parser.add_argument('--num_inducing', default=10, type=int,
+    parser.add_argument('--num_inducing', default=10, type=int, metavar='N',
                         help='Suggested number of inducing inputs (datasets don\'t have to use it)')
     parser.add_argument('--optimizer', default='Adam', help='Optimizer to use for SGD')
     parser.add_argument('--model_name', default='local',
                         help='Name of model (used for name of checkpoints)')
-    parser.add_argument('--batch_size', default=500, type=int, help='Batch size')
+    parser.add_argument('--batch_size', default=500, type=int, metavar='N', help='Batch size')
     parser.add_argument('--epochs', default=50, type=int, help='Number of epochs for training')
     parser.add_argument('--eval_epochs', default=1, help='Number of epochs between evaluations')
-    parser.add_argument('--summary_steps', default=100, type=int,
+    parser.add_argument('--summary_steps', default=100, type=int, metavar='N',
                         help='How many steps between saving summary')
-    parser.add_argument('--chkpnt_steps', default=5000, type=int,
+    parser.add_argument('--chkpnt_steps', default=5000, type=int, metavar='N',
                         help='How many steps between saving checkpoints')
     parser.add_argument('--save_dir', default='',
                         help='Directory where the checkpoints and summaries are saved (or \'\')')
     parser.add_argument('--plot', default='', help='Which function to use for plotting (or \'\')')
-    parser.add_argument('--logging_steps', default=10, type=int,
+    parser.add_argument('--logging_steps', default=10, type=int, metavar='N',
                         help='How many steps between logging the loss')
     parser.add_argument('--gpus', default='0',
                         help='Which GPUs to use (should normally only be one)')
     parser.add_argument('--preds_path', default='',
                         help='Path where the predictions for the test data will be save (or "")')
-    parser.add_argument('--eval_throttle', default=600, type=int,
+    parser.add_argument('--eval_throttle', default=600, type=int, metavar='N',
                         help='How long to wait before evaluating in seconds')
-    parser.add_argument('--lr_drop_steps', default=0, type=int,
+    parser.add_argument('--lr_drop_steps', default=0, type=int, metavar='N',
                         help='Number of steps before doing a learning rate drop')
     parser.add_argument('--lr_drop_factor', default=0.2, type=float,
                         help='For learning rate drop multiply by this factor')
-    parser.add_argument('--manual_seed', type=int,
+    parser.add_argument('--manual_seed', type=int, metavar='N',
                         help='manual seed, if not given resorts to random seed.')
 
     # Variational inference
-    parser.add_argument('--num_components', default=1, type=int,
+    parser.add_argument('--num_components', default=1, type=int, metavar='N',
                         help='Number of mixture of Gaussians components')
-    parser.add_argument('--num_samples', default=100, type=int,
+    parser.add_argument('--num_samples', default=100, type=int, metavar='N',
                         help='Number of samples for mean and variance estimate of likelihood')
     parser.add_argument('--diag_post', default=False, type=str2bool,
                         help='Whether the posterior is diagonal or not')
@@ -60,7 +60,7 @@ def parse_arguments(raw_args=None):
                         help='Whether to use the LOO (leave one out) loss (for hyper parameters)')
 
     # Likelihood
-    parser.add_argument('--num_samples_pred', default=2000, type=int,
+    parser.add_argument('--num_samples_pred', default=2000, type=int, metavar='N',
                         help='Number of samples for mean and variance estimate for prediction')
 
     # Fairness
